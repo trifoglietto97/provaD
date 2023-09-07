b57de6fe78f52b01e3cb6c26a58acf2e9e3f8f08
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-07 11:12:10 +0200

    initial commit

---
+++ src/test/java/com/example/provad/ProvaDApplication.java
@@ -0,0 +1,14 @@
+package com.example.provad;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provad/ProvaDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
